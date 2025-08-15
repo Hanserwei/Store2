4b62de0996bd4fd51bbb65688fddd0e1998b3176
Hanserwei <hanserwei@qq.com>
2025-08-15 20:20:55 +0800

    强迫症，所有mapper都有mapper接口，虽然没啥用

--- src/main/java/com/hanserwei/mapper/CategoriesMapper.java
+++ src/main/java/com/hanserwei/mapper/CategoriesMapper.java
@@ -1,9 +1,11 @@
 package com.hanserwei.mapper;
 
 import com.hanserwei.entity.vo.CategoryVO;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface CategoriesMapper {
     List<CategoryVO> selectAll();
 }
